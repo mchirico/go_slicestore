d4d8da17199924a0dbb0e42d616a6a2ab51fd8e2
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-03-16 11:52:08 -0400

    url

--- cmd/getdata/main.go
+++ cmd/getdata/main.go
@@ -1,45 +1,15 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
 	"github.com/mchirico/go_slicestore/pkg"
-	"github.com/mchirico/go_slicestore/yamlpkg"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
 )
 
 func main() {
 	d := pkg.DS{}
-	d.GetStruct(basicAuth())
+	q := pkg.Query{}
+	d.GetStruct(q.Query())
 	fmt.Printf("%v", d.ResponseData.VaultUsageReport.Vaults[0].Name)
 
 }
 
-func basicAuth() []byte {
-
-	y := yamlpkg.Config{}
-	file := fmt.Sprintf("%s/sliceStore.yaml", os.Getenv("HOME"))
-	y.Read(file)
-
-	url := fmt.Sprintf(
-		"https://%s/manager/api/json/1.0/vaultUsageReport.adm?dateRange=true&startDate=03/01/2019&endDate=03/02/2019",
-		y.Yaml.IP)
-
-	var username = y.Yaml.Username
-	var passwd = y.Yaml.Password
-	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	req.SetBasicAuth(username, passwd)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bodyText, err := ioutil.ReadAll(resp.Body)
-
-	return bodyText
-}
