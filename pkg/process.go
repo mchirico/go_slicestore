56bb33338cb2d305e82f369cfcc9b5225a9715be
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-03-16 16:46:47 -0400

    add do

--- pkg/process.go
+++ pkg/process.go
@@ -5,13 +5,42 @@ import (
 	"log"
 )
 
-func Process(data []byte) (DS, error)  {
 
-	s := DS{}
-	err := json.Unmarshal(data, &s)
+type ProcessStruct struct {
+	origname string
+	formatname string
+	order  int
+}
+
+
+type Pdata struct {
+	name []ProcessStruct
+	raw []DS
+}
+
+
+
+func (p *Pdata) Add(data []byte)   {
+
+	ds := DS{}
+	err := json.Unmarshal(data, &ds)
 	if err != nil {
 		log.Printf("Can't Unmarshal in pkg.process")
 	}
 
-	return s,err
+	ps := ProcessStruct{}
+	for _,v := range ds.ResponseData.VaultUsageReport.Vaults {
+		ps.origname = v.Name
+	}
+
+	p.name = append(p.name,ps)
+	p.raw = append(p.raw, ds)
+
+
+
+}
+
+
+func SortNames() {
+
 }
