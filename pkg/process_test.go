34db7757b1ff05a4c0e8e7400f2667d6290f2601
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-03-16 16:46:47 -0400

    add do

--- pkg/process_test.go
+++ pkg/process_test.go
@@ -2,30 +2,67 @@ package pkg
 
 import (
 	"io/ioutil"
+	"log"
+	"os"
 	"testing"
 )
 
-func TestProcess(t *testing.T) {
+var p  Pdata
+
+
+func TestMain(m *testing.M) {
+	SetupFunction()
+	retCode := m.Run()
+	TeardownFunction()
+	os.Exit(retCode)
+}
+
+func SetupFunction() {
+
+	p = Pdata{}
 	data, err := ioutil.ReadFile("../fixtures/" +
 		"fixtures_01_01_2019_01_02_2019")
 
+	data2, err := ioutil.ReadFile("../fixtures/" +
+		"fixtures_01_02_2019_01_03_2019")
+
 	if err != nil {
 
-		t.Fatalf("Error ioutil.ReadFile")
+		log.Fatalf("Error ioutil.ReadFile")
 	}
-
-	s, err := Process(data)
-	if err != nil {
-		t.FailNow()
-	}
-
-	if len(s.ResponseData.VaultUsageReport.Vaults) != 90 {
-		t.Fatalf("Problem with read. Expected 90")
-	}
-
+	p.Add(data)
+	p.Add(data2)
 
 }
 
+func TeardownFunction() {
+
+}
+
+
+func TestProcess(t *testing.T) {
+
+	if len(p.raw[0].ResponseData.VaultUsageReport.Vaults) != 90 {
+		t.Fatalf("Problem with read. Expected 90")
+	}
+
+	if len(p.raw) != 2 {
+		t.Fatalf("Problem with p.raw  Expected 2")
+	}
+
+}
+
+func TestProcess2(t *testing.T) {
+
+	if len(p.raw[0].ResponseData.VaultUsageReport.Vaults) != 90 {
+		t.Fatalf("Problem with read. Expected 90")
+	}
+
+	if len(p.raw) != 2 {
+		t.Fatalf("Problem with p.raw  Expected 2")
+	}
+
+}
 
 
 
