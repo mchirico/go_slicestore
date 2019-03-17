a779341a82dd1524f6eb63b6fe1adc7b73603c0f
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-03-16 20:22:23 -0400

    test

--- cmd/getdata/main.go
+++ cmd/getdata/main.go
@@ -1,24 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"github.com/mchirico/go_slicestore/pkg"
 )
 
 func main() {
-	d := pkg.DS{}
-	q := pkg.Query{}
-	d.GetStruct(q.Query())
-	fmt.Printf("%v", d.ResponseData.VaultUsageReport.Vaults[0].Name)
 
-	// Get a list for fixture to test
+	q := pkg.Query{}
 	pkg.DateFormat = "01/02/2006"
 	dt := pkg.DateType{}
 
-	dt.Year = 2019
-	dt.Month = 1
+	dt.Year = 2018
+	dt.Month = 7
 	dt.Day = 1
-	dt.AddDaysToList(10)
+	dt.AddDaysToList(258)
 	q.QueryList(dt)
 
 }
